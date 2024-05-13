5eb13289098d96ce7c5317d104f664d2f7938cd3
Talha Hussain <talhahussain349@gmail.com>
2024-05-13 22:43:30 +0500

    GET Api in Movies Controller Integration

--- Controller/movies.js
+++ Controller/movies.js
@@ -3,3 +3,10 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Movie = require('../models/movie');
 
+// GET
+router.get('/', function (req, res, next) {
+
+  mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true })
+    .then(() => console.log('connected to the database'))
+    .catch(err => console.error('error connecting to db: ', err.errmsg));
+
