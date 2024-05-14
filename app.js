825549daa12003e6c7c2d255ccc9384a9ed499a1
Talha Hussain <talhahussain349@gmail.com>
2024-05-15 00:36:42 +0500

    App.Js file creation with importing the mongoose schema and other necessary libraries

---
+++ app.js
@@ -0,0 +1,9 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var dotenv = require('dotenv').config()
+var logger = require('morgan');
+
+
+
