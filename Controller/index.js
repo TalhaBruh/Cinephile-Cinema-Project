a81207920bac2bd30bd4b5435ec455ddf72e610a
Talha Hussain <talhahussain349@gmail.com>
2024-05-15 00:29:56 +0500

    Get call to Get Movie from Db

--- Controller/index.js
+++ Controller/index.js
@@ -9,3 +9,15 @@ router.get('/explore', function(req, res, next) {
     mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true })
       .then(() => console.log('connected to the database'))
       .catch(err => console.error('error connecting to db: ', err.errmsg));
+      Movie
+    .find()
+    .sort('name')
+    .then(movies => {
+      res.render('index', { title: 'movie page', movies });
+    }).catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving notes."
+      });
+    });
+
+});
