b65a41376c6cc552d9fdd48d1e8243f7272a7101
Talha Hussain <talhahussain349@gmail.com>
2024-05-16 10:53:52 +0500

    Homepage Connection

--- app.js
+++ app.js
@@ -29,3 +29,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Calling FrontEnd Home Page
+app.get('/',(req,res)=>{
+	res.render("home.ejs")
+})
