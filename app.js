8de035f5983827bb1127a79b9722943885e3f7a3
Talha Hussain <talhahussain349@gmail.com>
2024-05-16 10:56:10 +0500

    Error handling

--- app.js
+++ app.js
@@ -47,3 +47,18 @@ app.get('/sign',(req,res)=>{
 app.get('/trailers',(req,res)=>{
 	res.render("trailers.ejs");
 })
+
+// Error handling Mechanism
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
