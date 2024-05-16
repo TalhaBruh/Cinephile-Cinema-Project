cfafd1664734b5c2395d766593cec9688746ea10
Talha Hussain <talhahussain349@gmail.com>
2024-05-16 10:53:06 +0500

    Indentation and formatting

--- app.js
+++ app.js
@@ -5,15 +5,11 @@ var cookieParser = require('cookie-parser');
 var dotenv = require('dotenv').config()
 var logger = require('morgan');
 
-
 // Mongoose and schema
-
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
 // Load the Movie model
-
 var Movie = require('./models/movie');
 var indexRouter = require('./Controller/index');
 var usersRouter = require('./Controller/users');
