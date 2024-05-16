1580fd7792f8f4141cc029152c0ead2eb37e9264
Talha Hussain <talhahussain349@gmail.com>
2024-05-16 10:52:42 +0500

    Cookie Parser

--- app.js
+++ app.js
@@ -26,3 +26,10 @@ console.log("starting")
 // Connecting the FrontEnd
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// Using the Express Cookie Parser
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
