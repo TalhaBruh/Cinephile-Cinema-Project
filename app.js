aebb5dd40be0791ccb3ac508f69fc3c13b27bca6
Talha Hussain <talhahussain349@gmail.com>
2024-05-17 23:27:45 +0500

    updated app.js

--- app.js
+++ app.js
@@ -2,65 +2,61 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var dotenv = require('dotenv').config()
+var dotenv = require('dotenv').config();
 var logger = require('morgan');
+var connectDB = require('./db');
 
-// Mongoose and schema
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var moviesRouter = require('./routes/movies');
 
-// Load the Movie model
-var Movie = require('./models/movie');
-var indexRouter = require('./Controller/index');
-var usersRouter = require('./Controller/users');
-var moviesRouter = require('./Controller/movies');
-
-// Express Application
 var app = express();
+
+// Connect to the database
+connectDB();
+
 console.log("starting")
 
-// Connecting the FrontEnd
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Using the Express Cookie Parser
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Calling FrontEnd Home Page
-app.get('/',(req,res)=>{
-	res.render("home.ejs")
-})
+app.get('/', (req, res) => {
+  res.render("home.ejs");
+});
 
-// Routing the Controllers
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/movies', moviesRouter);
 
-app.get('/sign',(req,res)=>{
-	res.render("sign.ejs");
-})
-
-app.get('/trailers',(req,res)=>{
-	res.render("trailers.ejs");
-})
-
-// Error handling Mechanism
-app.use(function (req, res, next) {
-	next(createError(404));
+app.get('/sign', (req, res) => {
+  res.render("sign.ejs");
 });
 
-app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/trailers', (req, res) => {
+  res.render("trailers.ejs");
+});
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
